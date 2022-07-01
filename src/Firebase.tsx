6645dfb81b3c584c965c84746bdd24d523365409
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-07-01 02:37:22 +0200

    V1.0 Build

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 //Auth
 import {GoogleAuthProvider, getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,7 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 
 const provider = new GoogleAuthProvider();
